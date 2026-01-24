9a4f62ba74acc6159d4ffadfbb8ad54fb85b170d
Alex Nogueira <alex@devaction.com.br>
2026-01-24 16:56:32 -0300

    chore: branch development

--- rector.php
+++ rector.php
@@ -13,9 +13,9 @@ return RectorConfig::configure()
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
+        codingStyle: true,
         typeDeclarations: true,
         privatization: true,
         earlyReturn: true,
-        strictBooleans: true,
     )
     ->withPhpSets();
